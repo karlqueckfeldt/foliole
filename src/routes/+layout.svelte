fdd43012be78ef65095ac3d0e84409ead9e87832
karlqueckfeldt <quecken@gmail.com>
2023-07-23 15:39:20 +0200

    Add links to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,18 @@
       <slot />
     </main>
     <footer>
-      <p>Made by Karl Queckfeldt using SvelteKit.</p>
+      <p>
+        Made by Karl Queckfeldt using SvelteKit. <a
+          href="https://github.com/karlqueckfeldt/foliole"
+          target="_blank"
+          rel="noopener noreferrer">GitHub repository</a
+        >.
+        <a
+          href="https://www.npmjs.com/package/foliole"
+          target="_blank"
+          rel="noopener noreferrer">NPM page</a
+        >.
+      </p>
     </footer>
   </div>
 </div>
