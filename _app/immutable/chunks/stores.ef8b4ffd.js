1861ec3e5da24ac6677647684ba48b3d02766e2d
karlqueckfeldt <quecken@gmail.com>
2023-07-23 15:40:40 +0200

    Update site

---
+++ _app/immutable/chunks/stores.ef8b4ffd.js
@@ -0,0 +1,1 @@
+import"./index.a164cd80.js";import{d as e}from"./singletons.81b0f890.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
