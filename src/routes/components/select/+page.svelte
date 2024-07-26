d064db9e7ff0c92c25a8fc1ee2c4a3252a4905d1
Karl Queckfeldt <quecken@gmail.com>
2024-07-26 21:29:33 +0200

    Update select component styling

--- src/routes/components/select/+page.svelte
+++ src/routes/components/select/+page.svelte
@@ -35,16 +35,18 @@
   <div class="selects">
     <div>
       <label class="fo-form-label" for="video_games_select">Select</label>
-      <select
-        id="video_games_select"
-        name="video_games_select"
-        class="fo-form-select"
-      >
-        <option value="Age of Empires">Age of Empires</option>
-        <option value="Super Mario">Super Mario</option>
-        <option value="Tetris" disabled>Tetris</option>
-        <option value="World of Warcraft">World of Warcraft</option>
-      </select>
+      <div class="fo-form-select-container">
+        <select
+          id="video_games_select"
+          name="video_games_select"
+          class="fo-form-select"
+        >
+          <option value="Age of Empires">Age of Empires</option>
+          <option value="Super Mario">Super Mario</option>
+          <option value="Tetris" disabled>Tetris</option>
+          <option value="World of Warcraft">World of Warcraft</option>
+        </select>
+      </div>
       <CodeViewer code={selectCode} />
     </div>
 
@@ -52,17 +54,19 @@
       <label class="fo-form-label" for="video_games_disabled_select"
         >Disabled select</label
       >
-      <select
-        id="video_games_disabled_select"
-        name="video_games_disabled_select"
-        class="fo-form-select"
-        disabled
-      >
-        <option value="Age of Empires">Age of Empires</option>
-        <option value="Super Mario">Super Mario</option>
-        <option value="Tetris" selected>Tetris</option>
-        <option value="World of Warcraft">World of Warcraft</option>
-      </select>
+      <div class="fo-form-select-container">
+        <select
+          id="video_games_disabled_select"
+          name="video_games_disabled_select"
+          class="fo-form-select"
+          disabled
+        >
+          <option value="Age of Empires">Age of Empires</option>
+          <option value="Super Mario">Super Mario</option>
+          <option value="Tetris" selected>Tetris</option>
+          <option value="World of Warcraft">World of Warcraft</option>
+        </select>
+      </div>
       <CodeViewer code={disabledSelectCode} />
     </div>
   </div>
